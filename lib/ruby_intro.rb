89cf4833819a1855311634f3dc21a4705477303c
Black Spirit <panagiotis_11@hotmail.com>
2015-11-01 11:09:57 +0000

    solved

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,78 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  num = 0
+  arr.each do |number|
+    num += number
+  end
+  return num
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  num = 0
+  if arr.length > 0
+    arr.sort!
+    num += arr.pop
+    if arr.length > 1
+      num += arr.pop
+    end
+  end
+  return num
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  i = 0
+  arr.each do |x|
+    j = 0
+    arr.each do |y|
+      if (x + y) == n and i != j
+        return true
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length > 0
+    return !(['a', 'e', 'i', 'o', 'u', '#'].include? s[0].downcase)
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  while s[0] == '0' and s.length > 1 do
+    s = s[1,s.length]
+  end
+  if s.to_i(2).to_s(2) == s
+    return s.to_i(2) % 4 == 0
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  def initialize isbn, price
+    if isbn.length == 0 or price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    return "$%.2f" % @price
+  end
 end
